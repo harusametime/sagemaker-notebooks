29a5263e742f2df4e50cb874e6e406655dd201e6
Masaki Samejima <samejim@amazon.co.jp>
2018-12-10 21:04:57 +0900

    Fix import error

--- keras_tensorflow/src/keras_cnn_mnist.py
+++ keras_tensorflow/src/keras_cnn_mnist.py
@@ -3,7 +3,8 @@ import keras
 import os
 import numpy as np
 
-from keras.models import Sequential,  save_mxnet_model
+import tensorflow as tf
+from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten
 from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
